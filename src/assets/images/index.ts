05184bed5c46640aee319f97fef098bd03030715
CookiePawn <cookiepawn@naver.com>
2025-06-04 14:52:48 +0900

    [add][image][add roulette images]

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,3 +6,7 @@ export { default as Guide4 } from './guide_4.png';
 export { default as Guide5 } from './guide_4-1.png';
 export { default as Guide6 } from './guide_5.png';
 export { default as Guide7 } from './guide_6.png';
+
+// 룰렛 이미지
+export { default as Roulette1 } from './roulette_1.png';
+export { default as Roulette2 } from './roulette_2.png';
